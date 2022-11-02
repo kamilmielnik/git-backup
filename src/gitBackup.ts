db609a5fd7191018cb3f4ca855514d0ac19d911d
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2022-11-03 02:51:35 +0700

    Extract gitBackup

---
+++ src/gitBackup.ts
@@ -0,0 +1,24 @@
+const copydir = require('copy-dir');
+const mkdirp = require('mkdirp');
+
+const exec = require('./exec');
+const isGitRepository = require('./isGitRepository');
+
+const gitBackup = async (srcDir, dstDir) => {
+  await mkdirp(dstDir);
+
+  if (!isGitRepository(dstDir)) {
+    exec(`git init`);
+  }
+
+  copydir.sync(srcDir, dstDir, {
+    utimes: true,
+    mode: true,
+    cover: true
+  });
+
+  exec('git add .', dstDir);
+  exec(`git commit -m "${new Date().toISOString()}"`, dstDir);
+};
+
+module.exports = gitBackup;
